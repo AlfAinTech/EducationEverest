045085184c93b934d1ec6957ff640728acb21bba
AbdullahMaud <abdullah.maud@kics.edu.pk>
2018-03-08 10:12:51 +0500

    some created issues in last merge. code missed

---
+++ EducationEverest/App_Code/dboExtentions/Personal_Details.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Personal_Details
+/// </summary>
+public partial class Personal_Details
+{
+   public string DateOfBirth
+    {
+        set { }
+        get
+        {
+           if(DOB != null)
+            {
+               return this.DOB.Value.ToString("dd-mm-yyyy", CultureInfo.InvariantCulture);
+            }
+           else
+            {
+                return "";
+            }
+        }
+    }
+}
