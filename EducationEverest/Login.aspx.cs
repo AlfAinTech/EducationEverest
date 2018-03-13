c5939c94ba392fe58ef1d08a2b4efc73f97e79bd
AlfainTech <www.alfain@gmail.com>
2018-03-13 15:12:34 +0500

    Login Issue

--- EducationEverest/Login.aspx.cs
+++ EducationEverest/Login.aspx.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -22,52 +23,23 @@ public partial class Login : System.Web.UI.Page
     {
         if (IsValid)
         {
+            
             // Validate the user password
             var manager = new UserManager();
             ApplicationUser user = manager.Find(Email.Text, Password.Text);
             if (user != null)
             {
-               
-                IdentityHelper.SignIn(manager, user, true);
-                if (HttpContext.Current.User.Identity.IsAuthenticated)
+                
+                IdentityHelper.SignIn(manager, user, false);
+                if (Request.QueryString["ReturnUrl"] != null)
                 {
-                    if (Request.QueryString["ReturnUrl"] != null)
-                    {
-                        if (HttpContext.Current.User.IsInRole("Super Admin"))
-                        {
-                            IdentityHelper.RedirectToReturnUrl("~/Admin/Applications.aspx", Response);
-                        }
-                        else
-                        {
-                            if (Request.QueryString["ReturnUrl"].Contains("Admin"))
-                            {
-                                IdentityHelper.RedirectToReturnUrl("~/Dashboard.aspx", Response);
-                            }
-                            else
-                            {
-                                IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
-                            }
-
-                        }
-
-                    }
-                    else
-                    {
-                        if (HttpContext.Current.User.IsInRole("Super Admin"))
-                        {
-                            IdentityHelper.RedirectToReturnUrl("~/Admin/Applications.aspx", Response);
-                        }
-                        else
-                        {
-                            IdentityHelper.RedirectToReturnUrl("~/Dashboard.aspx", Response);
-                        }
-                    }
+                    IdentityHelper.RedirectToReturnUrl("~/Dashboard.aspx?ReturnUrl=" + Request.QueryString["ReturnUrl"], Response);
                 }
                 else
                 {
-                    FailureText.Text = "Something went wrong , please try again";
-                    ErrorMessage.Visible = true;
+                    IdentityHelper.RedirectToReturnUrl("~/Dashboard.aspx", Response);
                 }
+                    
                 
             }
             else
