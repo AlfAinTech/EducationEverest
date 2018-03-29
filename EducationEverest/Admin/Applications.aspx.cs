e8733f4cd5d93f43ce8bf5cd423ac6738bdcbc16
AlfainTech <www.alfain@gmail.com>
2018-03-29 12:33:01 +0500

    Isharptext library removed

--- EducationEverest/Admin/Applications.aspx.cs
+++ EducationEverest/Admin/Applications.aspx.cs
@@ -6,8 +6,6 @@ using System.Net.Mail;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.IO;
-using iTextSharp.text;
 
 public partial class Applications : System.Web.UI.Page
 {
